6f9f01f68c39fd7e06989c611eefeeb2f32f8edd
ARMEYAYA <141784034+ARMEYAYA@users.noreply.github.com>
2026-05-22 11:42:00 +0800

    Setup server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,10 @@
 import express from "express";
+import path from "path";
+import brcypt from "brcypt";
+
+const app = express();
+
+
+app.listen(5000, () => {
+    console.log("Server is running on Port: 5000")
+});
